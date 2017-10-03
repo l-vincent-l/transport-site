3c41e46898fc4911602dfbae19db39fe72dcedae
l-vincent-l <lara.vincent@gmail.com>
2017-10-03 18:50:15 +0200

    Add user_organizations

--- lib/transport_web/controllers/page_controller.ex
+++ lib/transport_web/controllers/page_controller.ex
@@ -1,5 +1,6 @@
 defmodule TransportWeb.PageController do
   use TransportWeb, :controller
+  alias Transport.Datagouvfr.Client
 
   def index(conn, _params) do
     render conn, "index.html"
@@ -16,5 +17,4 @@ defmodule TransportWeb.PageController do
   def shortlist(conn, _) do
       render conn, "shortlist.html"
   end
-
 end
